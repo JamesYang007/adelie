bcaf6ada775175f5893189099ec1f23bc5457147
James Yang <jamesyang916@gmail.com>
2023-03-07 23:11:00 -0800

    Add current writeup and NMAB modification

--- glstudy/inst/include/fista.hpp
+++ glstudy/inst/include/fista.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstddef>
 
 namespace glstudy {
     
@@ -16,6 +17,7 @@ auto fista_solver(
 )
 {
     const auto lip = L.maxCoeff();
+    const auto nu = 1. / lip;
     const auto p = L.size();
     Eigen::VectorXd x_diff(p);
     Eigen::VectorXd x_old(p);
@@ -24,25 +26,77 @@ auto fista_solver(
     Eigen::VectorXd v_tilde(p);
     ValueType t = 1;
 
-    /*
-     * p_L(y) = (L+l_2)^{-1} (1-l_1 / ||v_tilde||_2)_+ v_tilde
-     * v_tilde = L (y - 1/L df(y))
-     */ 
     iters = 0;
     for (; iters < max_iters; ++iters) {
         x_old.swap(x);
-        v_tilde.array() = lip * (y.array() - 1./lip * (L.array() * y.array() - v.array()));
+        v_tilde.array() = y.array() - nu * (L.array() * y.array() - v.array());
         const auto v_tilde_l2 = v_tilde.norm();
-        if (v_tilde_l2 <= l1) {
+        if (v_tilde_l2 <= l1 * nu) {
             x.setZero();
         } else {
-            x = 1./(lip + l2) * (1 - l1 / v_tilde_l2) * v_tilde;
+            x = (lip /(lip + l2)) * (1 - (l1 * nu) / v_tilde_l2) * v_tilde;
         }
         const auto t_old = t;
         t = (1 + std::sqrt(1 + 4 * t * t)) * 0.5;
         x_diff = x - x_old;
         y = x + (t_old - 1) / t * x_diff;
         
+        if ((x_diff.array().abs() < tol * x.array().abs()).all()) break;
+    }
+    
+    x_sol = x;
+}
+
+/*
+ * FISTA with adaptive restart.
+ */
+template <class LType, class VType, class ValueType, class XType>
+inline
+auto fista_adares_solver(
+    const LType& L,
+    const VType& v,
+    ValueType l1,
+    ValueType l2,
+    ValueType tol,
+    size_t max_iters,
+    XType& x_sol,
+    size_t& iters
+)
+{
+    const auto lip = L.maxCoeff();
+    const auto nu = 1. / lip;
+    const auto p = L.size();
+    Eigen::VectorXd x_diff(p);
+    Eigen::VectorXd x_old(p);
+    Eigen::VectorXd x(p); x.setZero();
+    Eigen::VectorXd y(p); y = x;
+    Eigen::VectorXd v_tilde(p);
+    Eigen::VectorXd df(p);
+    ValueType t = 1;
+
+    iters = 0;
+    for (; iters < max_iters; ++iters) {
+        x_old.swap(x);
+        df.array() = (L.array() * y.array() - v.array());
+        v_tilde.array() = y.array() - nu * df.array();
+        const auto v_tilde_l2 = v_tilde.norm();
+        if (v_tilde_l2 <= l1 * nu) {
+            x.setZero();
+        } else {
+            x = (lip /(lip + l2)) * (1 - (l1 * nu) / v_tilde_l2) * v_tilde;
+        }
+        x_diff = x - x_old;
+        
+        // accelerated
+        if (v_tilde.dot(x_diff) > 0) {
+            y = x;
+            t = 1;
+        } else {
+            const auto t_old = t;
+            t = (1 + std::sqrt(1 + 4 * t * t)) * 0.5;
+            y = x + (t_old - 1) / t * x_diff;
+        }
+
         if ((x_diff.array().abs() < tol * x.array().abs()).all()) break;
     }
     
--- glstudy/src/rcpp_newton.cpp
+++ glstudy/src/rcpp_newton.cpp
@@ -33,3 +33,44 @@ Rcpp::List newton_solver(
         Rcpp::Named("iters")=iters
     );
 }
+
+//' Newton solver with more information.
+//'
+//' @param   L       vector representing a diagonal PSD matrix.
+//'                  Must have max(L + s) > 0. 
+//'                  L.size() <= buffer1.size().
+//' @param   v       any vector.  
+//' @param   l1      L2-norm penalty. Must be >= 0.
+//' @param   l2      L2 penalty. Must be >= 0.
+//' @param   tol         Newton's method tolerance of closeness to 0.
+//' @param   max_iters   maximum number of iterations of Newton's method.
+//' @export
+// [[Rcpp::export]]
+Rcpp::List newton_solver_debug(
+    const Eigen::Map<Eigen::VectorXd>& L,
+    const Eigen::Map<Eigen::VectorXd>& v,
+    double l1,
+    double l2,
+    double tol,
+    size_t max_iters,
+    bool smart_init=true
+)
+{
+    double h_min = 0;
+    double h_max = std::numeric_limits<double>::infinity();
+    std::vector<double> iters;
+    iters.reserve(max_iters);
+    std::vector<double> smart_iters;
+    smart_iters.reserve(max_iters);
+    Eigen::VectorXd x(L.size());
+    Eigen::VectorXd buffer1(L.size());
+    Eigen::VectorXd buffer2(L.size());
+    glstudy::newton_solver_debug(L, v, l1, l2, tol, max_iters, smart_init, h_min, h_max, x, iters, smart_iters, buffer1, buffer2);
+    return Rcpp::List::create(
+        Rcpp::Named("beta")=x,
+        Rcpp::Named("h_min")=h_min,
+        Rcpp::Named("h_max")=h_max,
+        Rcpp::Named("iters")=iters,
+        Rcpp::Named("smart_iters")=smart_iters
+    );
+}
