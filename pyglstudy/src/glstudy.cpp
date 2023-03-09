c4b9ee1c9134d11cdc464517ff7d76ce3f30b6d9
James Yang <jamesyang916@gmail.com>
2023-03-09 12:00:57 -0800

    Add debug mode stuff export and start changing newton examples

--- pyglstudy/src/glstudy.cpp
+++ pyglstudy/src/glstudy.cpp
@@ -1,5 +1,6 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
+#include <pybind11/stl.h>
 #include <newton.hpp>
 #include <fista.hpp>
 
@@ -41,6 +42,39 @@ py::dict newton_abs_solver(
     glstudy::newton_abs_solver(L, v, l1, l2, tol, max_iters, x, iters, buffer1, buffer2);
 
     py::dict d("beta"_a=x, "iters"_a=iters);
+    return d;
+}
+
+py::dict newton_solver_debug(
+    Eigen::Ref<Eigen::VectorXd> L,
+    Eigen::Ref<Eigen::VectorXd> v,
+    double l1,
+    double l2,
+    double tol,
+    size_t max_iters,
+    bool smart_init
+)
+{
+    double h_min, h_max;
+    Eigen::VectorXd x(L.size());
+    Eigen::VectorXd buffer1(L.size());
+    Eigen::VectorXd buffer2(L.size());
+    std::vector<double> iters;
+    iters.reserve(L.size());
+    std::vector<double> smart_iters;
+    smart_iters.reserve(L.size());
+    glstudy::newton_solver_debug(
+        L, v, l1, l2, tol, max_iters, true, 
+        h_min, h_max, x, iters, smart_iters, buffer1, buffer2
+    );
+    
+    py::dict d(
+        "beta"_a=x,
+        "h_min"_a=h_min,
+        "h_max"_a=h_max,
+        "iters"_a=iters,
+        "smart_iters"_a=smart_iters
+    );
     return d;
 }
 
@@ -95,6 +129,7 @@ py::dict fista_adares_solver(
 PYBIND11_MODULE(pyglstudy_ext, m) {
     m.def("newton_solver", &newton_solver);
     m.def("newton_abs_solver", &newton_abs_solver);
+    m.def("newton_solver_debug", &newton_solver_debug);
     m.def("ista_solver", &ista_solver);
     m.def("fista_solver", &fista_solver);
     m.def("fista_adares_solver", &fista_adares_solver);
