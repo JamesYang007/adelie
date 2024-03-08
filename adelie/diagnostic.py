Remove <3.11 req, completely fix IRLS sensitivity to hess near 0 and multinomial hessian majorization.

adelie/diagnostic.py
@@ -96,12 +96,11 @@ def predict(
     L = betas.shape[0]
 
     etas = np.empty((L,) + y_shape, order="C")
-    ones = np.ones(X.rows())
     if isinstance(betas, np.ndarray):
         for i in range(etas.shape[0]):
-            X.btmul(0, X.cols(), betas[i], ones, etas[i].ravel())
+            X.btmul(0, X.cols(), betas[i], etas[i].ravel())
     elif isinstance(betas, csr_matrix):
-        X.sp_btmul(betas, ones, etas.reshape((L, -1))) 
+        X.sp_btmul(betas, etas.reshape((L, -1))) 
     else:
         raise RuntimeError("beta is not one of np.ndarray or scipy.sparse.csr_matrix.")
     etas += intercepts[:, None] + offsets
@@ -353,8 +352,9 @@ def gradients(
         grad_shape = (X.cols(),)
 
     grads = np.empty((resids.shape[0],) + grad_shape)
+    ones = np.ones(np.prod(resids.shape[1:]))
     for i in range(grads.shape[0]):
-        X.mul(resids[i].ravel(), grads[i].ravel())
+        X.mul(resids[i].ravel(), ones, grads[i].ravel())
     return grads
 
 