7d59235e5641b64b8f46d3e2df5662e3d7562d17
James Yang <jamesyang916@gmail.com>
2024-03-10 13:11:11 -0700

    Add mac wheel with pre-installed openmp

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ from setuptools import setup, find_packages
 from distutils.dir_util import copy_tree
 from pybind11.setup_helpers import Pybind11Extension 
 from pybind11.setup_helpers import ParallelCompile
-import sysconfig
 import os
+import pathlib
 import platform
+import shutil
 import subprocess
+import sysconfig
 
 
 def run_cmd(cmd):
@@ -36,9 +38,11 @@ extra_compile_args += [
 ]
 libraries = []
 extra_link_args = []
+runtime_library_dirs = []
 
 system_name = platform.system()
 if (system_name == "Darwin"):
+    # check if OpenMP is installed
     no_omp_msg = (
         "OpenMP is not detected. "
         "MacOS users should install Homebrew and run 'brew install libomp' "
@@ -50,15 +54,38 @@ if (system_name == "Darwin"):
         raise RuntimeError(no_omp_msg)
     if "Not installed" in libomp_info:
         raise RuntimeError(no_omp_msg)
+
+    # grab include and lib directory
     omp_prefix = run_cmd("brew --prefix libomp")
     omp_include = os.path.join(omp_prefix, "include")
     omp_lib = os.path.join(omp_prefix, "lib")
+
+    # copy libomp.dylib to lib
+    adelie_lib = "adelie/lib"
+    pathlib.Path(adelie_lib).mkdir(parents=False, exist_ok=True)
+    omp_name = "libomp.dylib"
+    source_path = os.path.join(omp_lib, omp_name)
+    target_path = os.path.join(adelie_lib, omp_name)
+    shutil.copyfile(source_path, target_path)
+
+    # change rpath of libomp.dylib
+    run_cmd(
+        "install_name_tool -id "
+        f"@rpath/lib/{omp_name} "
+        f"{adelie_lib}/{omp_name}"
+    )
+    # as of Big Sur, we must codesign after the change.
+    # https://stackoverflow.com/questions/71744856/install-name-tool-errors-on-arm64
+    run_cmd(f"codesign --force -s - {adelie_lib}/{omp_name}")
+
+    # augment arguments
     extra_compile_args += [
         f"-I{omp_include}",
         "-Xclang",
         "-fopenmp",
     ]
-    extra_link_args += [f'-L{omp_lib}']
+    extra_link_args += [f'-L{adelie_lib}']
+    runtime_library_dirs = ["@loader_path"]
     libraries = ['omp']
     
 if (system_name == "Linux"):
@@ -77,6 +104,7 @@ ext_modules = [
         ],
         extra_compile_args=extra_compile_args,
         extra_link_args=extra_link_args,
+        runtime_library_dirs=runtime_library_dirs,
         libraries=libraries,
         cxx_std=17, # TODO: changed from 14 to 17 just because of tqdm
     ),
@@ -97,6 +125,7 @@ setup(
             "src/**/*.hpp", 
             "src/third_party/**/*",
             "adelie_core.cpython*",
+            "lib/*.dylib",
         ],
     },
     ext_modules=ext_modules,
