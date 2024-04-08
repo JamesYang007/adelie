c51964cd70f31c5d8623aa53fa596157e68104e7
James Yang <jamesyang916@gmail.com>
2024-04-08 11:17:17 -0400

    Change proximal to take AAT for majorization

--- adelie/src/include/adelie_core/bcd/constrained/proximal_newton.hpp
+++ adelie/src/include/adelie_core/bcd/constrained/proximal_newton.hpp
@@ -8,7 +8,7 @@ namespace bcd {
 namespace constrained {
 
 template <class QuadType, class LinearType, class ValueType,
-          class AType, class BType, class ATVarsType,
+          class AType, class BType, class ATVarsType, class AATType,
           class OutType, class BuffType>
 void proximal_newton_solver(
     const QuadType& quad,
@@ -18,6 +18,7 @@ void proximal_newton_solver(
     const AType& A,
     const BType& b,
     const ATVarsType& AT_vars,
+    const AATType& AAT,
     size_t max_iters,
     ValueType tol,
     size_t newton_max_iters,
@@ -61,6 +62,14 @@ void proximal_newton_solver(
     bool is_prev_valid = false;
     bool recompute_mu_resid = true;
 
+    // TODO: move this out and check performance.
+    // cache AS^{-1}A^T
+    {
+        // must add some regularization
+        hess.array() = A.array().rowwise() / (S + l2);
+        hess_buff.noalias() = hess * A.transpose();
+    }
+
     while (iters < max_iters) {
         ++iters;
 
@@ -82,11 +91,30 @@ void proximal_newton_solver(
 
         // If ||x||_2 = 0, check if 0 is a valid solution.
         if (x_norm <= 0) {
+            // check convergence
+            if (is_prev_valid) {
+                const auto convg_meas = std::abs(
+                    ((mu-mu_prev) * (grad_prev+b)).sum()
+                ) / m;
+                if (convg_meas <= tol) return;
+            }
+
+            // save old values
+            mu_prev = mu;
+            grad_prev = -b;
+            is_prev_valid = true;
+
+            // The proximal operator simplifies to solving:
+            //      minimize_{mu >= 0} -b^T mu
+            // This means we set mu[i] = 0 whenever b[i] > 0, 
+            // and mu_resid must be updated accordingly.
             for (int i = 0; i < m; ++i) {
                 if (b[i] <= 0 || mu[i] <= 0) continue;
                 mu_resid += mu[i] * A.row(i).array();
                 mu[i] = 0;
             }
+            recompute_mu_resid = false;
+
             size_t nnls_iters;
             value_t mu_loss = 0.5 * mu_resid.square().sum();
             optimization::nnls_naive(
@@ -98,16 +126,8 @@ void proximal_newton_solver(
 
             // If mu_loss is smaller than or close to l1, 
             // then check passed and 0 is a valid primal solution.
+            // TODO: generalize this constant.
             if (mu_loss <= l1 * (0.5+5e-5)) return;
-
-            // Otherwise, 0 is probably not the solution.
-            // The proximal operator simplifies to solving:
-            //      minimize_{mu >= 0} -b^T mu
-            // This means we set mu[i] = 0 whenever b[i] > 0, 
-            // and mu_resid must be updated accordingly.
-            // Note that this was exactly done beforehand!
-            // Now, just continue with the updated mu :).
-            recompute_mu_resid = false;
             continue;
         }
 
@@ -120,19 +140,31 @@ void proximal_newton_solver(
             const auto convg_meas = std::abs(
                 ((mu-mu_prev) * (grad_prev-grad)).sum()
             ) / m;
-            if (convg_meas <= tol) break;
+            if (convg_meas <= tol) return;
         }
 
         // compute hessian
         // x_buffer1 = quad + l2
-        // x_buffer2 = x_buffer1 * x_norm + lmda
-        hess_buff.array() = A.array().rowwise() / x_buffer2;
-        hess.noalias() = hess_buff * A.transpose();
-        alpha_tmp = x / (x_norm * x_buffer2);
+        // x_buffer2 = 1 / (x_buffer1 * x_norm + lmda)
+
+        // TODO: generalize constant
+        // If x_norm is sufficiently small compared to l1,
+        // then x_norm * A ((S+l2) x_norm + l1)^{-1} A^T is negligible,
+        // so just approximate by setting it to 0.
+        //alpha_tmp = x_norm * x_buffer2;
+        //hess_buff.array() = A.array().rowwise() * alpha_tmp;
+        //hess.noalias() = hess_buff * A.transpose();
+
+        // Method 2: poor approximation
+        //hess = (x_norm * x_buffer2.maxCoeff()) * AAT;
+
+        // Method 3: cache ASA^T
+        hess = (1 - l1 * x_buffer2).maxCoeff() * hess_buff;
+
+        alpha_tmp = (x * x_buffer2) / x_norm;
         alpha.matrix().noalias() = alpha_tmp.matrix() * A.transpose();
-        const auto kappa = 1 / (1 - l1 * (x * alpha_tmp).sum() / x_norm);
-        hess.noalias() += l1 * kappa * alpha.matrix().transpose() * alpha.matrix();
-        hess.array() *= x_norm;
+        const auto l1_kappa_norm = l1 * x_norm / (x * x_buffer1 * alpha_tmp).sum();
+        hess.noalias() += l1_kappa_norm * alpha.matrix().transpose() * alpha.matrix();
 
         // save old values
         mu_prev = mu;
@@ -148,6 +180,17 @@ void proximal_newton_solver(
         ); 
         recompute_mu_resid = true;
     }
+
+    // compute v - A^T mu
+    if (recompute_mu_resid) {
+        mu_resid.matrix().noalias() = mu.matrix() * A;
+        mu_resid = v - mu_resid;
+    }
+    size_t x_iters;
+    unconstrained::newton_abs_solver(
+        quad, mu_resid, l1, l2, newton_tol, newton_max_iters, 
+        x, x_iters, x_buffer1, x_buffer2
+    );
 }
 
 } // namespace constrained
