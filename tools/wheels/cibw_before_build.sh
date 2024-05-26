b0a2aad7ed5a285adf5ba5db4298a7c2373e1ec5
James Yang <jamesyang916@gmail.com>
2024-05-26 14:12:13 -0700

    Add micromamba

--- tools/wheels/cibw_before_build.sh
+++ tools/wheels/cibw_before_build.sh
@@ -3,36 +3,38 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     # See https://github.com/scikit-learn/scikit-learn/blob/09781c540077a7f1f4f2392c9287e08e479c4f29/build_tools/wheels/build_wheels.sh#L18-L50
     # See https://cibuildwheel.pypa.io/en/stable/faq/#macos-library-dependencies-do-not-satisfy-target-macos
     if [[ "$CIBW_BUILD" == *-macosx_arm64 ]]; then
-        # install conda
-        set -ex
-        # macos arm64 runners do not have conda installed. Thus we much install conda manually
-        EXPECTED_SHA="dd832d8a65a861b5592b2cf1d55f26031f7c1491b30321754443931e7b1e6832"
-        MINIFORGE_URL="https://github.com/conda-forge/miniforge/releases/download/23.11.0-0/Mambaforge-23.11.0-0-MacOSX-arm64.sh"
-        curl -L --retry 10 $MINIFORGE_URL -o miniforge.sh
+        ## install conda
+        #set -ex
+        ## macos arm64 runners do not have conda installed. Thus we much install conda manually
+        #EXPECTED_SHA="dd832d8a65a861b5592b2cf1d55f26031f7c1491b30321754443931e7b1e6832"
+        #MINIFORGE_URL="https://github.com/conda-forge/miniforge/releases/download/23.11.0-0/Mambaforge-23.11.0-0-MacOSX-arm64.sh"
+        #curl -L --retry 10 $MINIFORGE_URL -o miniforge.sh
 
-        # Check SHA
-        file_sha=$(shasum -a 256 miniforge.sh | awk '{print $1}')
-        if [ "$EXPECTED_SHA" != "$file_sha" ]; then
-            echo "SHA values did not match!"
-            exit 1
-        fi
+        ## Check SHA
+        #file_sha=$(shasum -a 256 miniforge.sh | awk '{print $1}')
+        #if [ "$EXPECTED_SHA" != "$file_sha" ]; then
+        #    echo "SHA values did not match!"
+        #    exit 1
+        #fi
 
-        # Install miniforge
-        MINIFORGE_PATH=$HOME/miniforge
-        bash ./miniforge.sh -b -p $MINIFORGE_PATH
-        PATH="$PATH:$MINIFORGE_PATH/bin"
-        CONDA_HOME=$MINIFORGE_PATH
+        ## Install miniforge
+        #MINIFORGE_PATH=$HOME/miniforge
+        #bash ./miniforge.sh -b -p $MINIFORGE_PATH
+        #PATH="$PATH:$MINIFORGE_PATH/bin"
+        #CONDA_HOME=$MINIFORGE_PATH
 
         export MACOSX_DEPLOYMENT_TARGET=12.0
-        OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-arm64/llvm-openmp-11.1.0-hf3c4609_1.tar.bz2"
+        #OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-arm64/llvm-openmp-11.1.0-hf3c4609_1.tar.bz2"
     else
         # Non-macos arm64 envrionments already have conda installed
-        echo "CONDA_HOME=/usr/local/miniconda" >> $GITHUB_ENV
+        #CONDA_HOME="/usr/local/miniconda"
 
         export MACOSX_DEPLOYMENT_TARGET=10.9
-        OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-64/llvm-openmp-11.1.0-hda6cdc1_1.tar.bz2"
+        #OPENMP_URL="https://anaconda.org/conda-forge/llvm-openmp/11.1.0/download/osx-64/llvm-openmp-11.1.0-hda6cdc1_1.tar.bz2"
     fi
 
-    sudo conda create -n build $OPENMP_URL
-    export OPENMP_PREFIX="$CONDA_HOME/envs/build"
+    #sudo conda create -n build $OPENMP_URL
+    #export OPENMP_PREFIX="$CONDA_HOME/envs/build"
+    set -ex
+    echo $OPENMP_PREFIX
 fi
