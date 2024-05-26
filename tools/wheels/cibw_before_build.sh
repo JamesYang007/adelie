d25d0378d2c474a380b16f44d7122c0e5d8c46cf
James Yang <jamesyang916@gmail.com>
2024-05-26 09:53:33 -0700

    Add MACOSX_DEPLOYMENT_TARGET

--- tools/wheels/cibw_before_build.sh
+++ tools/wheels/cibw_before_build.sh
@@ -2,4 +2,11 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     brew install libomp
+
+    # See https://cibuildwheel.pypa.io/en/stable/faq/#macos-library-dependencies-do-not-satisfy-target-macos
+    if [[ "$CIBW_BUILD" == *-macosx_arm64 ]]; then
+        export MACOSX_DEPLOYMENT_TARGET=14.0
+    else
+        export MACOSX_DEPLOYMENT_TARGET=13
+    fi
 fi
