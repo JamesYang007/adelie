f8729628e765622bf834d22f96bff010110cc35d
James Yang <jamesyang916@gmail.com>
2024-05-26 16:50:40 -0700

    Add KMP_DUPLICATE_LIB_OK like sklearn

--- adelie/__init__.py
+++ adelie/__init__.py
@@ -12,14 +12,15 @@ import os
 # ---------------------------------------------------------------
 # Set thread block time (polling) to be sufficiently large.
 # It is very costly when OMP puts threads to sleep.
-if not ("KMP_BLOCKTIME" in os.environ):
-    os.environ["KMP_BLOCKTIME"] = "1" # in ms
-if not ("GOMP_SPINCOUNT" in os.environ):
-    os.environ["GOMP_SPINCOUNT"] = "1000000"
+os.environ.setdefault("KMP_BLOCKTIME", "1") # in ms
+os.environ.setdefault("GOMP_SPINCOUNT", "1000000")
+
+# Allow duplicate libs by default.
+# See https://github.com/scikit-learn/scikit-learn/blob/09781c540077a7f1f4f2392c9287e08e479c4f29/sklearn/__init__.py#L53
+os.environ.setdefault("KMP_DUPLICATE_LIB_OK", "True")
 
 # Bind each thread to a single core to localize memory.
-if not ("OMP_PROC_BIND" in os.environ):
-    os.environ["OMP_PROC_BIND"] = "TRUE"
+os.environ.setdefault("OMP_PROC_BIND", "TRUE")
 
 from . import adelie_core
 from . import bcd
