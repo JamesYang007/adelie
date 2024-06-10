5aff244df05fc1c9ae9da301da3977d1e78dcf99
James Yang <jamesyang916@gmail.com>
2024-06-09 18:53:15 -0700

    Generalize IndexType for matrix derived classes

--- adelie/src/include/adelie_core/matrix/matrix_naive_interaction.hpp
+++ adelie/src/include/adelie_core/matrix/matrix_naive_interaction.hpp
@@ -5,11 +5,11 @@
 namespace adelie_core {
 namespace matrix {
 
-template <class DenseType>
-class MatrixNaiveInteractionDense: public MatrixNaiveBase<typename DenseType::Scalar>
+template <class DenseType, class IndexType=Eigen::Index>
+class MatrixNaiveInteractionDense: public MatrixNaiveBase<typename DenseType::Scalar, IndexType>
 {
 public:
-    using base_t = MatrixNaiveBase<typename DenseType::Scalar>;
+    using base_t = MatrixNaiveBase<typename DenseType::Scalar, IndexType>;
     using typename base_t::value_t;
     using typename base_t::index_t;
     using typename base_t::vec_value_t;
