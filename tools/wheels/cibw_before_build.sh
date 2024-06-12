e303d04a9b8da382e4d41cf41f8e8f483471bf53
James Yang <jamesyang916@gmail.com>
2024-06-12 00:43:01 -0700

    Try adding as environment to CIBW

--- tools/wheels/cibw_before_build.sh
+++ tools/wheels/cibw_before_build.sh
@@ -9,25 +9,4 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     else
         export MACOSX_DEPLOYMENT_TARGET=10.9
     fi
-elif [[ "$OSTYPE" == "linux"* ]]; then
-    # Linux build is fully self-contained, so extra care is needed.
-    # The host file system is mounted as /host (https://cibuildwheel.pypa.io/en/stable/faq/).
-    # We can access MAMBA_ROOT_PREFIX of host for the conda environment.
-    export MAMBA_ROOT_PREFIX="/host/home/runner/micromamba"
-    ls $MAMBA_ROOT_PREFIX
-
-    ## Install wget
-    #yum install wget
-
-    ## Install miniconda
-    #mkdir -p ~/miniconda3
-    #wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
-    #bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
-    #rm -rf ~/miniconda3/miniconda.sh
-    #export PATH=$PATH:~/miniconda3/bin
-    #conda init bash
-
-    ## Create adelie environment
-    #conda create -n adelie eigen==3.4.0
-    #conda activate adelie
 fi
