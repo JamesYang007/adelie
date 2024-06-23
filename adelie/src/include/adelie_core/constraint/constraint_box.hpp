fe30baa6d6f0192a5efc0f39f7332af7799f5811
James Yang <jamesyang916@gmail.com>
2024-06-23 14:19:05 -0700

    Add hard check optimization, change error message, verify that soft-check in ellipse case is crucial, and start box.

--- adelie/src/include/adelie_core/constraint/constraint_box.hpp
+++ adelie/src/include/adelie_core/constraint/constraint_box.hpp
@@ -1,14 +1,13 @@
 #pragma once
 #include <adelie_core/constraint/constraint_base.hpp>
 #include <adelie_core/bcd/unconstrained/newton.hpp>
-#include <adelie_core/optimization/nnqp_full.hpp>
 #include <adelie_core/util/macros.hpp>
 
 namespace adelie_core {
 namespace constraint {
 
 template <class ValueType>
-class ConstraintOneSidedBase: public ConstraintBase<ValueType>
+class ConstraintBoxBase: public ConstraintBase<ValueType>
 {
 public:
     using base_t = ConstraintBase<ValueType>;
@@ -18,25 +17,25 @@ public:
     using map_cvec_value_t = Eigen::Map<const vec_value_t>;
 
 protected:
-    const map_cvec_value_t _sgn;
-    const map_cvec_value_t _b;
+    const map_cvec_value_t _l;
+    const map_cvec_value_t _u;
 
 public:
-    explicit ConstraintOneSidedBase(
-        const Eigen::Ref<const vec_value_t> sgn,
-        const Eigen::Ref<const vec_value_t> b
+    explicit ConstraintBoxBase(
+        const Eigen::Ref<const vec_value_t> l,
+        const Eigen::Ref<const vec_value_t> u
     ):
-        _sgn(sgn.data(), sgn.size()),
-        _b(b.data(), b.size())
+        _l(l.data(), l.size()),
+        _u(u.data(), u.size())
     {
-        if ((_sgn.abs() != 1).any()) {
-            throw util::adelie_core_error("sgn must be a vector of +/-1.");
+        if (_u.size() != _l.size()) {
+            throw util::adelie_core_error("u and l must have the same length.");
         }
-        if ((_b < 0).any()) {
-            throw util::adelie_core_error("b must be >= 0.");
+        if ((_u < 0).any()) {
+            throw util::adelie_core_error("u must be >= 0.");
         }
-        if (_sgn.size() != _b.size()) {
-            throw util::adelie_core_error("sgn and b must have the same length.");
+        if ((_l < 0).any()) {
+            throw util::adelie_core_error("l must be >= 0.");
         }
     }
 
@@ -51,25 +50,33 @@ public:
         const Eigen::Ref<const colmat_value_t>& Q
     ) const
     {
-        const auto A = _sgn[0] * Q(0,0);
-        const auto b = _b[0];
+        const auto A = Q(0,0);
+        const auto u = _u[0];
+        const auto l = _l[0];
         const auto q = quad[0];
         const auto v = linear[0];
 
         // check if x == 0 is optimal
-        auto mu0 = (b > 0) ? 0 : std::max<value_t>(A * v, 0.0);
+        auto mu0_pos = (u > 0) ? 0 : std::max<value_t>(A * v, 0.0);
+        auto mu0_neg = (l > 0) ? 0 : std::max<value_t>(-A * v, 0.0);
+        auto mu0 = mu0_pos - mu0_neg;
         const auto is_zero_opt = std::abs(v - A * mu0) <= l1;
 
         // if optimal, take previous solution, else compute general solution
         const auto x0 = is_zero_opt ? 0 : (
-            A * std::min(
+            A * std::max(std::min(
                 A * std::copysign(std::abs(v) - l1, v) / (q + l2), 
-                b
-            )
+                u, 
+            ), -l)
         );
-        mu0 = is_zero_opt ? mu0 : (
-            (A * x0 < b) ? 0 : (A * (v - ((q + l2) * x0 + std::copysign(l1, x0))))
+        const auto mu0_full = A * (v - ((q + l2) * x0 + std::copysign(l1, x0)));
+        mu0_pos = is_zero_opt ? mu0_pos : (
+            (A * x0 < u) ? 0 : std::max<value_t>(mu0_full, 0)
         );
+        mu0_neg = is_zero_opt ? mu0_neg : (
+            (A * x0 > -l) ? 0 : std::max<value_t>(-mu0_full, 0)
+        );
+        mu0 = mu0_pos - mu0_neg;
 
         // store output
         x[0] = x0;
@@ -80,7 +87,7 @@ public:
         Eigen::Ref<vec_value_t> x
     ) override
     {
-        x = _sgn * (_sgn * x).min(_b);
+        x = (x).min(_u).max(_l);
     }
 
     void gradient(
@@ -89,24 +96,24 @@ public:
         Eigen::Ref<vec_value_t> out
     ) override
     {
-        out = _sgn * mu;
+        out = mu;
     }
 
-    int duals() override { return _b.size(); }
-    int primals() override { return _b.size(); }
+    int duals() override { return _u.size(); }
+    int primals() override { return _u.size(); }
 };
 
 template <class ValueType>
-class ConstraintOneSidedProximalNewton: public ConstraintOneSidedBase<ValueType>
+class ConstraintBoxProximalNewton: public ConstraintBoxBase<ValueType>
 {
 public:
-    using base_t = ConstraintOneSidedBase<ValueType>;
+    using base_t = ConstraintBoxBase<ValueType>;
     using typename base_t::value_t;
     using typename base_t::vec_value_t;
     using typename base_t::colmat_value_t;
     using typename base_t::map_cvec_value_t;
-    using base_t::_sgn;
-    using base_t::_b;
+    using base_t::_l;
+    using base_t::_u;
 
 private:
     const size_t _max_iters;
@@ -119,15 +126,10 @@ private:
     const value_t _slack;
     vec_value_t _buff;
 
-#ifdef ADELIE_CORE_DEBUG
-    std::vector<vec_value_t> _primals;
-    std::vector<vec_value_t> _duals;
-#endif
-
 public:
-    explicit ConstraintOneSidedProximalNewton(
-        const Eigen::Ref<const vec_value_t> sgn,
-        const Eigen::Ref<const vec_value_t> b,
+    explicit ConstraintBoxProximalNewton(
+        const Eigen::Ref<const vec_value_t> l,
+        const Eigen::Ref<const vec_value_t> u,
         size_t max_iters,
         value_t tol,
         size_t nnls_max_iters,
@@ -135,14 +137,14 @@ public:
         value_t cs_tol,
         value_t slack
     ):
-        base_t(sgn, b),
+        base_t(l, u),
         _max_iters(max_iters),
         _tol(tol),
         _nnls_max_iters(nnls_max_iters),
         _nnls_tol(nnls_tol),
         _cs_tol(cs_tol),
         _slack(slack),
-        _buff((b.size() <= 1) ? 0 : (b.size() * (9 + 2 * b.size())))
+        _buff((b.size() <= 1) ? 0 : (0))
     {
         if (tol < 0) {
             throw util::adelie_core_error("tol must be >= 0.");
@@ -158,20 +160,6 @@ public:
         }
     }
 
-    auto debug_info() const 
-    {
-        #ifdef ADELIE_CORE_DEBUG
-        util::rowmat_type<value_t> pr(_primals.size(), _b.size());
-        util::rowmat_type<value_t> du(_duals.size(), _b.size());
-        for (size_t i = 0; i < _primals.size(); ++i) 
-        {
-            pr.row(i) = _primals[i];
-            du.row(i) = _duals[i];
-        }
-        return std::make_tuple(pr, du); 
-        #endif
-    }
-
     void solve(
         Eigen::Ref<vec_value_t> x,
         Eigen::Ref<vec_value_t> mu,
@@ -184,12 +172,7 @@ public:
     {
         using rowmat_value_t = util::rowmat_type<value_t>;
 
-        #ifdef ADELIE_CORE_DEBUG
-        _primals.clear();
-        _duals.clear();
-        #endif
-        
-        const auto m = _b.size();
+        const auto m = _u.size();
         const auto d = m;
 
         if (d == 1) {
@@ -219,7 +202,7 @@ public:
         value_t mu_resid_norm_prev = -1;
 
         const auto compute_mu_resid = [&]() {
-            mu_resid.matrix() = v.matrix() - (_sgn * mu).matrix() * Q;
+            mu_resid.matrix() = v.matrix() - mu.matrix() * Q;
         };
         const auto compute_primal = [&]() {
             size_t x_iters;
@@ -228,17 +211,6 @@ public:
                 x, x_iters, x_buffer1, x_buffer2
             );
         };
-        #ifdef ADELIE_CORE_DEBUG
-        const auto save_iterate = [&]() {
-            _primals.push_back(x);
-            _duals.push_back(mu);
-            if (Eigen::isnan(x).any() || Eigen::isnan(mu).any()) {
-                PRINT(x);
-                PRINT(mu);
-                throw util::adelie_core_error("Found nan!");
-            }
-        };
-        #endif
 
         while (iters < _max_iters) {
             ++iters;
@@ -368,10 +340,14 @@ public:
             save_iterate();
             #endif
 
-            grad.matrix() = (x.matrix() * Q.transpose()).cwiseProduct(_sgn.matrix()) - _b.matrix();
+            grad.matrix() = x.matrix() * Q.transpose();
 
             // optimization: if optimality hard-check is satisfied, finish early.
-            if ((grad <= 0).all() && (mu * grad == 0).all()) return;
+            if (
+                ((grad <= _u) && (grad >= -_l)).all() && 
+                (mu.max(0) * (grad - _u) == 0).all() &&
+                (mu.min(0) * (grad - _l) == 0).all()
+            ) return;
 
             // Check if mu is not changing much w.r.t. hessian scaling.
             if (is_prev_valid) {
@@ -386,10 +362,6 @@ public:
             mu_prev = mu;
             grad_prev = grad;
             is_prev_valid = true;
-
-            // reparametrize
-            grad *= _sgn;
-            mu *= _sgn;
 
             // Compute hessian
             // NOTE:
@@ -423,189 +395,6 @@ public:
         }
 
         throw util::adelie_core_solver_error("ConstraintOneSidedProximalNewton: max iterations reached!");
-    }
-};
-
-template <class ValueType>
-class ConstraintOneSidedADMM: public ConstraintOneSidedBase<ValueType>
-{
-public:
-    using base_t = ConstraintOneSidedBase<ValueType>;
-    using typename base_t::value_t;
-    using typename base_t::vec_value_t;
-    using typename base_t::colmat_value_t;
-    using typename base_t::map_cvec_value_t;
-    using base_t::_sgn;
-    using base_t::_b;
-
-private:
-    const size_t _max_iters;
-    const value_t _tol_abs;
-    const value_t _tol_rel;
-    const size_t _newton_max_iters = 100000;
-    const value_t _newton_tol = 1e-12;
-    const value_t _rho;
-    vec_value_t _buff;
-
-#ifdef ADELIE_CORE_DEBUG
-    std::vector<vec_value_t> _primals1;
-    std::vector<vec_value_t> _primals2;
-    std::vector<vec_value_t> _duals;
-#endif
-
-public:
-    explicit ConstraintOneSidedADMM(
-        const Eigen::Ref<const vec_value_t> sgn,
-        const Eigen::Ref<const vec_value_t> b,
-        size_t max_iters,
-        value_t tol_abs,
-        value_t tol_rel,
-        value_t rho
-    ):
-        base_t(sgn, b),
-        _max_iters(max_iters),
-        _tol_abs(tol_abs),
-        _tol_rel(tol_rel),
-        _rho(rho),
-        _buff((b.size() <= 1) ? 0 : (7 * b.size()))
-    {
-        if (tol_abs < 0) {
-            throw util::adelie_core_error("tol_abs must be >= 0.");
-        }
-        if (tol_rel < 0) {
-            throw util::adelie_core_error("tol_rel must be >= 0.");
-        }
-        if (rho <= 0) {
-            throw util::adelie_core_error("rho must be > 0.");
-        }
-    }
-
-    auto debug_info() const 
-    {
-        #ifdef ADELIE_CORE_DEBUG
-        util::rowmat_type<value_t> pr1(_primals1.size(), _b.size());
-        util::rowmat_type<value_t> pr2(_primals2.size(), _b.size());
-        util::rowmat_type<value_t> du(_duals.size(), _b.size());
-        for (size_t i = 0; i < _primals1.size(); ++i) 
-        {
-            pr1.row(i) = _primals1[i];
-            pr2.row(i) = _primals2[i];
-            du.row(i) = _duals[i];
-        }
-        return std::make_tuple(pr1, pr2, du); 
-        #endif
-    }
-
-    void solve(
-        Eigen::Ref<vec_value_t> x,
-        Eigen::Ref<vec_value_t> mu,
-        const Eigen::Ref<const vec_value_t>& quad,
-        const Eigen::Ref<const vec_value_t>& linear,
-        value_t l1,
-        value_t l2,
-        const Eigen::Ref<const colmat_value_t>& Q
-    ) override
-    {
-        using rowmat_value_t = util::rowmat_type<value_t>;
-
-        #ifdef ADELIE_CORE_DEBUG
-        _primals1.clear();
-        _primals2.clear();
-        _duals.clear();
-        #endif
-        
-        const auto m = _b.size();
-        const auto d = m;
-
-        if (d == 1) {
-            base_t::solve_1d(x, mu, quad, linear, l1, l2, Q);
-            return;
-        } 
-
-        const auto& v = linear;
-
-        size_t iters = 0;
-
-        auto buff_ptr = _buff.data();
-        Eigen::Map<vec_value_t> x_buffer1(buff_ptr, d); buff_ptr += d;
-        Eigen::Map<vec_value_t> x_buffer2(buff_ptr, d); buff_ptr += d;
-        Eigen::Map<vec_value_t> z(buff_ptr, m); buff_ptr += m;
-        Eigen::Map<vec_value_t> z_prev(buff_ptr, m); buff_ptr += m;
-        Eigen::Map<vec_value_t> u(buff_ptr, m); buff_ptr += m;
-        Eigen::Map<vec_value_t> linear_shifted(buff_ptr, d); buff_ptr += d;
-        Eigen::Map<vec_value_t> s(buff_ptr, m); buff_ptr += m;
-
-        // TODO: these warm-starts may not be optimal
-        z = ((x.matrix() * Q.transpose()).array() * _sgn).min(_b);
-        u.setZero();
-
-        const auto compute_primal_x = [&]() {
-            size_t x_iters;
-            bcd::unconstrained::newton_abs_solver(
-                quad, linear_shifted, l1, l2+_rho, _newton_tol, _newton_max_iters, 
-                x, x_iters, x_buffer1, x_buffer2
-            );
-        };
-
-        const auto compute_primal_z = [&]() {
-            z = linear_shifted.min(_b);
-        };
-
-        const auto compute_dual = [&]() {
-            const auto x_norm = x.matrix().norm();
-            if (x_norm <= 0) {
-                mu = (
-                    ((v.matrix() * Q.transpose()).array() * _sgn).max(0) 
-                    * (_b <= 0).template cast<value_t>()
-                );
-                return;
-            }
-            mu = (((v - (quad + l2 + l1 / x_norm) * x).matrix() * Q.transpose()).array() * _sgn).max(0);
-        };
-
-        #ifdef ADELIE_CORE_DEBUG
-        const auto save_iterate = [&]() {
-            _primals1.push_back(x);
-            _primals2.push_back(z);
-            _duals.push_back(mu);
-            if (Eigen::isnan(x).any() || Eigen::isnan(z).any() || Eigen::isnan(mu).any()) {
-                PRINT(x);
-                PRINT(z);
-                PRINT(mu);
-                throw util::adelie_core_error("Found nan!");
-            }
-        };
-        #endif
-
-        while (iters < _max_iters) {
-            ++iters;
-            linear_shifted.matrix() = v.matrix() + _rho * (_sgn * (z - u)).matrix() * Q;
-            compute_primal_x();
-            linear_shifted = (x.matrix() * Q.transpose()).array() * _sgn + u;
-            z_prev = z;
-            compute_primal_z();
-            auto& r = linear_shifted;
-            r = linear_shifted - z - u;
-            u += r;
-            s.matrix() = -_rho * (_sgn * (z-z_prev)).matrix() * Q;
-            const value_t eps_pri = (
-                std::sqrt(m) * _tol_abs
-                + _tol_rel * std::max(x.matrix().norm(), z.matrix().norm())
-            );
-            const value_t eps_dual = (
-                std::sqrt(d) * _tol_abs 
-                + _tol_rel * _rho * u.matrix().norm()
-            );
-            if (
-                (r.matrix().norm() <= eps_pri) &&
-                (s.matrix().norm() <= eps_dual)
-            ) {
-                compute_dual();
-                return;
-            }
-        }
-
-        throw util::adelie_core_solver_error("ConstraintOneSidedADMM: max iterations reached!");
     }
 };
 
