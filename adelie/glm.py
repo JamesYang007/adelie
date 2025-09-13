4dda9b6a5d8ac02d354a1113aff055e2452b3a0c
jrich129 <82070215+jrich129@users.noreply.github.com>
2025-09-13 15:33:44 -0700

    Edited Cox to have start default at a vector of zero

--- adelie/glm.py
+++ adelie/glm.py
@@ -197,10 +197,10 @@ def binomial(
 
 
 def cox(
-    start: np.ndarray,
     stop: np.ndarray,
     status: np.ndarray,
     *,
+    start: np.ndarray =None,
     strata: np.ndarray =None,
     weights: np.ndarray =None,
     tie_method: str ="efron",
@@ -287,12 +287,14 @@ def cox(
 
     Parameters
     ----------
-    start : (n,) ndarray
-        Start time vector :math:`s`.
     stop : (n,) ndarray
         Stop time vector :math:`t`.
     status : (n,) ndarray 
         Status vector :math:`\\delta`.
+    start : (n,) ndarray, optional
+        Start time vector :math:`s`.
+        If ``None``, it is set to a vector of zeros.
+        Default is ``None``.
     strata : (n,) ndarray, optional
         Strata vector :math:`S`.
         If ``None``, there is only one stratum.
@@ -335,6 +337,9 @@ def cox(
 
     core_base = dispatcher[dtype]
 
+    if start is None:
+        start = np.zeros(status.size, dtype=dtype)
+
     if strata is None:
         strata = np.zeros(status.size, dtype=int)
 
@@ -359,9 +364,9 @@ def cox(
         def reweight(self, weights=None):
             weights = self.weights if weights is None else weights
             return cox(
-                start=start, 
                 stop=stop,
                 status=status,
+                start=start,
                 strata=strata,
                 weights=weights, 
                 tie_method=tie_method,
